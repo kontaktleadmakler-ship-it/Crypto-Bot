8f6b3ce296eacd7e23d5def1af4d2d9a1b180a4c
kontaktleadmakler-ship-it <kontakt.leadmakler@gmail.com>
2026-09-07 23:20:08 +0200

    Add files via upload

--- dashboard/service-worker.js
+++ dashboard/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='jarvis-pwa-v1';
+const CACHE='jarvis-pwa-v2';
 const SHELL=['/dashboard','/dashboard/manifest.webmanifest','/dashboard/icons/icon-192.png','/dashboard/icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
